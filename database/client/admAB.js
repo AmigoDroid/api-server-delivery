c98b3b74f9d8e4f2814adb3768160d4395ffbd53
AmigoDroid <colteman05@gmail.com>
2021-11-06 17:05:43 -0200

    teste

--- database/client/admAB.js
+++ database/client/admAB.js
@@ -5,22 +5,22 @@ module.exports={
     async listar (req,res){
         const tokenAdmin = req.params.tokenAdmin;
         console.log("Token: "+tokenAdmin);
-        if(tokenAdmin==admin){
+        if(tokenAdmin===admin){
             const listaClientes = await cliente.findAll();
             return res.json(listaClientes).status(200);
         }else{
-            return res.status(404);
+            return res.send('erro');
         }
     },
     async cadastrar(req,res){
         const body = req.body;
         const tokenAdmin = req.params.tokenAdmin;;
         console.log("Token: "+tokenAdmin);
-        if (tokenAdmin == admin) {
+        if (tokenAdmin === admin) {
             const cadastre = await cliente.create(body);
             return res.json(body).status(200);
         }else{
-            return res.send('erro').status(404);
+            return res.send('erro');
         }
     }
 }
