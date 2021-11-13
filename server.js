873ecb4b200b45c9d06fd846dce062677ddfb006
AmigoDroid <colteman05@gmail.com>
2021-11-13 12:49:22 -0200

    cors acti

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const body = require('body-parser');
 const morgan = require('morgan');
-//const cors = require('cors');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 //VARIAVES
 const porta = process.env.PORT||8080;
 const rotas = require('./Routes');
@@ -16,26 +14,12 @@ const rotas = require('./Routes');
 app.use(morgan('dev'));
 app.use(body.urlencoded({extended:false}));
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 app.use(rotas);
 
 
-
-//soket.io
-app.get('/',(req,res)=>{
-    res.send('get');
-})
-
-io.on('connection',socket =>{
-//socket conection 
-console.log("Conectado: ID= "+socket.id);
-})
-
-
-
-
 //STARTAR SERVER
-server.listen(porta,()=>{
+app.listen(porta,()=>{
     console.log('');
     console.log("host: "+'localhost:'+porta);
     console.log('');
